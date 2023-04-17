bfa2e0d1a085e17bc8b3343dc3b95d9fdd147517
hardika-spec-610 <hardika.moradiya1996@gmail.com>
2023-04-17 16:50:43 +0200

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import express from "express"
+import { Server } from "socket.io"
+import { createServer } from "http" // CORE MODULE
+import cors from "cors"
+import { newConnectionHandler } from "./socket/index"
+import { badRequestHandler, forbiddenHandler, genericErrorHandler, notFoundHandler, unauthorizedHandler } from "./errorHandlers"
+
+const expressServer = express()
+
+// **************************** SOCKET.IO **************************
+const httpServer = createServer(expressServer)
+const socketioServer = new Server(httpServer) // this constructor expects to receive an HTTP-SERVER as parameter (NOT AN EXPRESS SERVER!!!!!)
+
+socketioServer.on("connection", newConnectionHandler)
+
+// *************************** MIDDLEWARES *************************
+expressServer.use(cors())
+expressServer.use(express.json())
+
+// *************************** ENDPOINTS ***************************
+
+// ************************* ERROR HANDLERS ************************
+expressServer.use(badRequestHandler)
+expressServer.use(unauthorizedHandler)
+expressServer.use(forbiddenHandler)
+expressServer.use(notFoundHandler)
+expressServer.use(genericErrorHandler)
+
+export { httpServer, expressServer }
---
+++ src/socket/index.ts
@@ -0,0 +1,13 @@
+import { Socket } from "socket.io"
+
+interface OnlineUser {
+  username: string
+  socketId: string
+}
+
+
+
+export const newConnectionHandler = (socket: Socket) => {
+ 
+
+}
