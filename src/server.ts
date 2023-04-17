223674bbcd5c753edf4e1cdb65ccf64fe676e1f2
ajeer333 <ajeer333@gmail.com>
2023-04-17 21:51:18 +0200

    commit

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,51 @@
-import express from "express"
-import { Server } from "socket.io"
-import { createServer } from "http" // CORE MODULE
-import cors from "cors"
-import { newConnectionHandler } from "./socket/index"
-import { badRequestHandler, forbiddenHandler, genericErrorHandler, notFoundHandler, unauthorizedHandler } from "./errorHandlers"
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http"; // CORE MODULE
+import cors from "cors";
+import { newConnectionHandler } from "./socket/index";
+import {
+  badRequestHandler,
+  forbiddenHandler,
+  genericErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers";
 
-const expressServer = express()
+const expressServer = express();
 
 // **************************** SOCKET.IO **************************
-const httpServer = createServer(expressServer)
-const socketioServer = new Server(httpServer) // this constructor expects to receive an HTTP-SERVER as parameter (NOT AN EXPRESS SERVER!!!!!)
+const httpServer = createServer(expressServer);
+const socketioServer = new Server(httpServer); // this constructor expects to receive an HTTP-SERVER as parameter (NOT AN EXPRESS SERVER!!!!!)
 
-socketioServer.on("connection", newConnectionHandler)
+socketioServer.on("connection", newConnectionHandler);
 
 // *************************** MIDDLEWARES *************************
-expressServer.use(cors())
-expressServer.use(express.json())
+expressServer.use(cors());
+expressServer.use(express.json());
 
 // *************************** ENDPOINTS ***************************
 
 // ************************* ERROR HANDLERS ************************
-expressServer.use(badRequestHandler)
-expressServer.use(unauthorizedHandler)
-expressServer.use(forbiddenHandler)
-expressServer.use(notFoundHandler)
-expressServer.use(genericErrorHandler)
+expressServer.use(badRequestHandler);
+expressServer.use(unauthorizedHandler);
+expressServer.use(forbiddenHandler);
+expressServer.use(notFoundHandler);
+expressServer.use(genericErrorHandler);
 
-export { httpServer, expressServer }
+export { httpServer, expressServer };
+
+// const expressServer = express();
+// const port = process.env.PORT;
+
+// const httpServer = createServer(expressServer);
+// const socketioServer = new Server(httpServer);
+
+// socketioServer.on("connection", newConnectionHandler);
+
+// mongoose.connect(process.env.MONGO_URL);
+
+// mongoose.connection.on("connected", () => {
+//   httpServer.listen(port, () => {
+//     console.log(`Server listening on port ${port}`);
+//   });
+// });
