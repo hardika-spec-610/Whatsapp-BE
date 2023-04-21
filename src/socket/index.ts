e17f3136e826b13604e79b99e70155cf551f31f0
Erik <erik.ondra@yahoo.com>
2023-04-21 15:00:14 +0200

    some changes  for socketio

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import { Socket } from "socket.io";
-
 import ChatModel from "../api/chats/model";
+
 interface payload {
   username: String;
   email: String;
@@ -22,15 +22,18 @@ let onlineUsers: user[] = [];
 export const newConnectionHandler = (socket: Socket) => {
   socket.emit("welcome", { message: `Hello ${socket.id}` });
 
-  socket.on("setUsername", (payload: payload) => {
-    onlineUsers.push({ username: payload.username, socketId: socket.id });
+  socket.on("enterChat", (chatId: string) => {
+    console.log("enterChat");
+    socket.join(chatId);
+    socket.emit("roomEntered", chatId);
   });
 
-  socket.emit("loggedIn", onlineUsers);
-
-  socket.on("sendMessage", async (message: message) => {
-    socket.broadcast.emit("newMessage", message);
+  socket.on("sendMessage", async (message: message & { chatId: string }) => {
+    console.log("sendMessage:", message);
     const newMessage = new ChatModel(message);
     const { _id } = await newMessage.save();
+    const savedMessage = await ChatModel.findById(_id);
+    console.log("savedMessage:", savedMessage);
+    socket.emit("newMessage", message);
   });
 };
